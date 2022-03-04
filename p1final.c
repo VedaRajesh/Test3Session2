403b0970c4675c827edb960a16b4eeb98ac6daa9
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-03-04 10:17:28 +0000

    4.3.2022

--- p1final.c
+++ p1final.c
@@ -19,7 +19,7 @@ void output(int num1, int den1, int num2, int den2, int num3, int den3)
             if(num3%i==0 && den3%i==0)
                 gcd=i;
         }
-    printf("\n sum of %d/%d + %d/%d is %d/%d",num1,den1,num2,den2,num3,den3);
+    printf("\n sum of %d/%d + %d/%d is %d/%d",num1,den1,num2,den2,num3/gcd,den3/gcd);
 }
 int main()
 {
--- p2final.c
+++ p2final.c
@@ -11,3 +11,30 @@ Fraction input_fraction()
     scanf("%d%d",&f.num,&f.den);
     return f;
 }
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{
+ int common_dinominator=f1.den * f2.den * f3.den;
+    int num1=f1.num * common_dinominator/f1.den;
+    int num2=f2.num * common_dinominator/f2.den;
+    int num3=f3.num * common_dinominator/f3.den;
+    if(num1 < num2 && num1 < num3)
+        return f1;
+    else if(num2 <num3)
+        return f2;
+    else 
+        return f3;
+}
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+{
+    printf("the smallest of %d/%d ,%d/%d,%d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,largest.num,largest.den);
+    }
+    int main()
+    {
+        Fraction f1,f2,f3,largest;
+        f1=input_fraction();
+        f2=input_fraction();
+        f3=input_fraction();
+        largest=Largest_fraction(f1,f2,f3);
+        output(f1,f2,f3,largest);
+        return 0;
+    }
