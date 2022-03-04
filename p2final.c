9fa0f91a580e1bdda62a3436a756cd1636b5bfaf
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-03-04 09:56:01 +0000

    4.3.2022

--- p2final.c
+++ p2final.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct _fraction Fraction;
+Fraction input_fraction()
+{
+    Fraction f;
+    printf("enter nr and dr\n");
+    scanf("%d%d",&f.num,&f.den);
+    return f;
+}
