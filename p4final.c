b22eb0b937fb29f18cc9ec20da15fe1fdc4f80df
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-03-07 07:06:23 +0000

    7.3.2022

--- p4final.c
+++ p4final.c
@@ -1,45 +1,26 @@
-/*
-
-Write a program to find nth number in fibonacci sequence.
-Fibonacci sequence consists of 0,1,1,2,3,5,8,13,21........
-int input();
-int find_fibo(int n);
-void output(int n, int fibo);
-
-*/
-
-#include <stdio.h>
-
-int input()
+float evaluate_polynomial(int n,float f[n],float x)
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+    float result=f[n-1];
+    for(int i=n-1;i>0;i--)
+        {
+            result=result * x;
+            result=result+f[i-1];
+        }
+    return result;
 }
-int find_fibo(int n)
+int fact(int n)
 {
-  int fibo=0;
-  int a=0;
-  int b=1;
-  for(int i=0;i<n;i++) {
-    fibo = a;
-    a = b;
-    b = fibo + b;
-  }
-  return fibo;
+    for (int i)
+}
+int findNcr(int n,int r)
+{
+    int result=fact(n)/(fact(n-r)*fact(r))
+    }
+int factMcr(int n,int r)
+{
+    result=1;
+    for(int i=1;i<=r;i++)
+        result=result * monkey/donkey;
+    return result;
 }
 
-
-void output(int n, int fibo)
-{
-  printf("the %dth fibonacci number is %d\n", n, fibo);
-}
-
-int main()
-{
-  int n = input();
-  int fibo = find_fibo(n);
-  output(n, fibo);
-  return 0;
-}
