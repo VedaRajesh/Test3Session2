6ed85a1419ce7f53ef2775df8cb2b2a579209ca7
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-03-11 10:02:01 +0000

    11.march.2022

--- p4final.c
+++ p4final.c
@@ -1,26 +1,49 @@
-float evaluate_polynomial(int n,float f[n],float x)
+#include <stdio.h>
+ int input_degree()
 {
-    float result=f[n-1];
-    for(int i=n-1;i>0;i--)
-        {
-            result=result * x;
-            result=result+f[i-1];
-        }
-    return result;
+    int n;
+    printf("input degree?");
+    scanf("%d",&n);
+    return n+1;
 }
-int fact(int n)
+float input_x()
 {
-    for (int i)
+    float x;
+    printf("enter x");
+    scanf("%f",&x);
+    return x;
 }
-int findNcr(int n,int r)
+void input_coeffecients(int n,float a[n])
 {
-    int result=fact(n)/(fact(n-r)*fact(r))
-    }
-int factMcr(int n,int r)
-{
-    result=1;
-    for(int i=1;i<=r;i++)
-        result=result * monkey/donkey;
-    return result;
-}
+    printf("input %d coeffecients \n",n);
 
+    for(int i=0;i<n;i++)
+        scanf("%f",&a[i]);
+}
+float evaluate_polynomial(int n, float a[n], float x)
+{
+    float rs = a[0];
+    for(int i=1;i<n;i+=1)
+        rs=rs*x + a[i];
+
+    return rs;
+}
+void output(int n,int a[n],int largest)
+{
+    printf("\nMAX between");
+    for(int i=0;i<n-1;i+=1)
+        printf("%d x^%d +",a[i],n-i-1);
+    printf("%f at x=%f\n",a[n-1],x);
+
+    printf("=%d\n",result);
+}
+int main()
+{
+    int n=input_degree();
+    float x=input_x();
+
+    float ar[n];
+    input_coeffecients(n,ar);
+
+    output(n,ar,x,evaluate_polynomial(n,ar,x));
+}
